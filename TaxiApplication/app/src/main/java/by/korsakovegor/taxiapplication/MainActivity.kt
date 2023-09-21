b33919266bbd0dceb786e25061d4ac7829111d05
D1le1 <egor20031500@gmail.com>
2023-09-21 12:59:22 +0300

    Start Work With TaxiApplication

---
+++ TaxiApplication/app/src/main/java/by/korsakovegor/taxiapplication/MainActivity.kt
@@ -0,0 +1,79 @@
+package by.korsakovegor.taxiapplication
+
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.FragmentActivity
+import by.korsakovegor.taxiapplication.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : OnMapReadyCallback, FragmentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var map: FrameLayout
+    private lateinit var currentLocation: Location
+    private lateinit var fusedClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fusedClient = LocationServices.getFusedLocationProviderClient(this)
+        getLocation()
+
+        binding.relocate.setOnClickListener { getLocation() }
+    }
+
+    private fun getLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101
+            )
+            return
+        }
+        var task = fusedClient.lastLocation
+        task.addOnSuccessListener {
+            if (it != null) {
+                currentLocation = it
+                var mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+                mapFragment.getMapAsync(this)
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        var mapBelarus = LatLng(currentLocation.latitude, currentLocation.longitude)
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(mapBelarus))
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mapBelarus, 12.0f))
+        googleMap.addMarker(MarkerOptions().position(mapBelarus).title("Client"))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 101)
+        {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                getLocation()
+        }
+    }
+}
